04c944d1f735ae96614343e9fde1dc6aeb8eb90d
victoria-savona <42382140+victoria-savona@users.noreply.github.com>
2018-08-21 13:46:53 -0400

    Problem set 1 question 1
    Tuition 1

---
+++ ProbSet 1 Q1.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |ProbSet 1 Q1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+If per term tuition is $25225, four classes per term, attendance thrice a week for thirteen weeks, cost of each class would be as follows.
+Four can be replaced with any number of classes taken if attendance pattern is static. 
+(/ (/ 25225 4) (* 3 13))
